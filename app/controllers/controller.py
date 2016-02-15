660c3a2070e883c9f7cfcc091385086dbc5d58fe
Pratyush More <Pratyush More>
2016-02-15 20:58:13 +0800

    Added hashing and method to add professor

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -5,23 +5,46 @@
     This module holds the Controller
     component of the app.
 """
-from app.models.professor import *
+from app.models.database_controller import *
+import hashlib
 
 class Controller:
     def __init__(self):
         self.data = DatabaseConnector()
 
     WRONG_CREDENTIALS = "Failed Login. Please check your entered credentials."
-    
-    def search_prof(self, name) :
+
+    def search_prof(self, name):
         result = self.data.search_professor("name", name)
         return result
 
     def authenticate(email, password):
-        isSuccess = self.data.authenticate(email, password)
+        hashed_pw = hash_password(password)
+        isSuccess = self.data.authenticate(email, hashed_pw)
         if isSuccess == true:
             return isSuccess[1]
         return WRONG_CREDENTIALS
 
-    def get_all_professors():
+    def get_all_professors(self):
         return self.data.get_all_professors()
+
+    def get_all_professor_names(self):
+        professor = self.get_all_professors()
+        return [prof.name for prof in professor]
+
+    def add_professor(self, prof_details):
+        password = prof_details["password"]
+        hashed_pw = hash_password(password)
+        prof_details["password"] = hashed_pw
+        self.data.add_professor(prof_details)
+
+    def hash_password(password):
+        hasher = hashlib.sha1()
+        hasher.update(password)
+        hashed_pw = hasher.hexdigest()
+        return hashed_pw
+
+    def update_value(self,email, param, value):
+        if param = "password":
+            value = hash_password(value)
+        self.data.update_professor(email, param, value)
